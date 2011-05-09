5bd41b86ace9bd6b3c1efdd83a6bb852af496bbd
adrian.wisniewski <adrian.wisniewski@localhost>
2011-05-09 21:47:18 +0200

    Change management

--- trunk/db/src/sql/insert.employee_roles.sql
+++
@@ -1,3 +0,0 @@
-insert into employee_role(employee_id, role_code) (select employee_id, role_code from employees, roles where login like '%' || role_code || '%');
-insert into employee_role(employee_id, role_code) (select employee_id, '2_LINE' from employees where login like '%GR%');
-insert into employee_role(employee_id, role_code) (select employee_id, 'PRB_GR' from employees where login like '%GR%');
